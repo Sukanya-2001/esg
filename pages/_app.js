792e149f21d714b8a0c2b3758b967721fcd2d881
Someswarklizos2001 <someswar@klizos.com>
2025-05-24 12:57:17 +0530

    seg

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
 
+if (typeof window !== "undefined") {
+  import("bootstrap/dist/js/bootstrap.bundle.min.js");
+}
 export default function App({ Component, pageProps }) {
+  
   return <Component {...pageProps} />;
 }
